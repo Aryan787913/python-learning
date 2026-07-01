cef5837cae9e9096162a91febd197993ee9dc507
Aaryan singh <157215207+Aryan787913@users.noreply.github.com>
2026-07-01 23:53:26 +0530

    Add files via upload

---
+++ special_character.py
@@ -0,0 +1,9 @@
+print('hi\"aryan') # \" use for double quote
+
+print('hi\'aryan') # \' use for Single quote
+
+print('hi\\aryan') # \\ use for backslash
+
+print('hi\naryan') # \" use for New line
+
+print('hi\taryan') # \" use for new tab

aae5593ec21273eabc1ccc033a86f58788d96dc2
Aaryan singh <157215207+Aryan787913@users.noreply.github.com>
2026-07-04 23:43:55 +0530

    Add files via upload

--- fun.py
+++ fun.py
@@ -4,5 +4,8 @@ ln = "singh"
 lnn = fn +" " + ln 
 print(lnn)
 #f-string
+print("=" * 100)
 print(f"my first name is {fn}, and last name is {ln}, and full name is {lnn}")
+print("=" * 100)
+
 print(f"{{my first name is {fn}, and last name is {ln}, and full name is {lnn}}}")
